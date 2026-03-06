f8c53324ddea69bc29413c255291e1fde90e0672
Wayne Ashley Berry <727262+wayneashleyberry@users.noreply.github.com>
2026-03-06 22:05:51 +0000

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,5 @@ module.exports = {
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
-    require("@tailwindcss/line-clamp"),
-    require("@tailwindcss/aspect-ratio"),
   ],
 };
